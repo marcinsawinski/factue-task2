06e8d2b553122806e5afa9f0bdefea9e5e0e276c
Marcin Sawinski <marcin.sawinski@gmail.com>
2025-04-24 17:45:37 +0200

    preprocess task

--- factue/utils/vars.py
+++ factue/utils/vars.py
@@ -4,5 +4,10 @@ from pathlib import Path
 from dotenv import load_dotenv
 
 load_dotenv()
-project_root = Path(os.getenv("PROJECT_ROOT"))
-duckdb_file = project_root / os.getenv("DUCKDB_FILE")
+
+project_root = Path(os.getenv("PROJECT_ROOT", "."))
+duckdb_file_str = os.getenv("DUCKDB_FILE")
+
+if duckdb_file_str is None:
+    raise ValueError("DUCKDB_FILE environment variable is not set")
+duckdb_file = project_root / duckdb_file_str
