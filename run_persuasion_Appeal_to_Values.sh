e2f652f35d7d4950bf6d03afcd84579d41c556cd
Marcin Sawinski <marcin.sawinski@gmail.com>
2025-05-04 16:40:28 +0200

    loaded prompt

---
+++ run_persuasion_Appeal_to_Values.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+# This script runs the lt_extract pipeline and passes any extra parameters to it
+
+python -m factue.pipelines.persuasion_detect \
+  --split train \
+  --max-iterations 1 \
+  --seed -1 \
+  --model-name LLAMA_31_8B\
+  --model-provider OLLAMA \
+  --model-mode CHAT \
+  --prompt-id "justification/Appeal_to_Values_v001"\
+  --part 0001 \
+  --lang pl \
+  "$@" 
